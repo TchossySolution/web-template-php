37d31910ad450cd40c2722f0a27d1f5d604ed2d9
Rafael Pilartes <rafaelpilartes.rlps@gmail.com>
2022-09-04 04:56:37 +0100

    Template -> Add bootstrap

--- app/helpers/constants.php
+++ app/helpers/constants.php
@@ -1,23 +1,38 @@
 <?php
 
 define('URL_BASE', "http://localhost/web-template-php");
-define('SITE', "Template");
+define('SITE', "Tv-One");
+
+define('CONTROLLERS', 'app/controllers');
 
 define('FOLDER_BASE', "/app/views/base");
 define('BASE_VIEWS', "/../views/base");
 define('BASE_STYLES', "/src/styles");
 define('BASE_JS', "/src/js");
-define('BASE_IMAGES', "/src/assets/images");
-define('BASE_ICONS', "/src/assets/icons");
+define('BASE_IMG', "/src/assets/images");
 
-define('FOLDER_DASHBOAR', "/app/views/base");
+define('BASE_DETAILSNEWS', "news/detailsNews");
+
+define('FOLDER_DASHBOARD', "/app/views/dashboard");
 define('DASHBOARD_VIEWS', "/../views/dashboard");
+define('DASHBOARD_STYLES', "/src/styles");
+define('DASHBOARD_JS', "/src/js");
+define('DASHBOARD_IMG', "/src/assets/images");
 
 
-function urlProject(string $uri = null): string{
-  if($uri){
+function urlProject(string $uri = null): string
+{
+  if ($uri) {
     return URL_BASE . "/{$uri}";
   }
 
   return URL_BASE;
+}
+function urlDashProject(string $uri = null): string
+{
+  if ($uri) {
+    return URL_BASE . "/dashboard/{$uri}";
+  }
+
+  return URL_BASE . "/dashboard";
 }
